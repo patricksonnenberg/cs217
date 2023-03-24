3ff2d28da3fe44cfd5cf56dc45cd3cf670f6e42f
patricksonnenberg <97453213+patricksonnenberg@users.noreply.github.com>
2023-03-24 15:43:11 -0400

    assignment2 addition

---
+++ assignment2/README.md
@@ -0,0 +1,53 @@
+##CS217 PA2
+This assignment creates a Flask webserver to access spaCy NER. There is a database on the backend to keep track of counts, and the application is dockerized.
+
+spaCy NER will tag all entities present in a text. There are various labels for entities, including person, location, and organization.
+
+### Flask Webserver
+This webserver allows a user to type their text in a textbox and click a button. The user will then be
+directed to a new page. The new page displays the result form the request to spaCy. The entities are marked
+in place in the sentence, and they are also listed individually. 
+
+##### To run (without Docker): 
+Run the flask_webserver.py file:
+```bash
+python flask_webserver.py
+```
+Go to the address (http://127.0.0.1:5000).
+
+##### To run (with Docker): 
+Create the Docker image by first navigating to the assignment2 folder in the terminal and running the command:
+```bash
+docker build -t myflaskapp .
+```
+
+This builds a Docker image called myflaskapp. Now run this command to start a container from this image:
+```bash
+docker run -p 5000:5000 myflaskapp
+```
+
+This maps port 5000 in the container to port 5000 on the host machine. Now go to http://localhost:5000 or http://127.0.0.1:5000 in a web browser to see the app. 
+
+To stop the container, manually stop it in the Docker app, or run the command:
+```bash
+docker ps
+```
+
+This will provide the name of the container, which can then be used in the command:
+```bash
+docker stop <name>
+```
+
+### Modules and Versions
+I used a conda environment and the following: 
+
+conda 4.14.0
+
+conda-build 3.21.5
+
+flask 2.2.2
+
+python 3.9.7
+
+spacy 3.3.1
+
---
+++ assignment2/flask_webserver.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template
+import spacy
+import ner
+import db
+
+app = Flask(__name__)
+nlp = spacy.load("en_core_web_sm")
+connection = db.DatabaseConnection('entities.sqlite')
+
+@app.route("/")
+def index():
+    return render_template('form.html')
+
+@app.route('/analyze', methods=["POST"])
+def show_result():
+    text = request.form["text_input"]
+    doc = nlp(text)
+    spacyed_text = ner.SpacyDocument(text)  # Creating spacy object
+    ner_spacyed_text = spacyed_text.get_entities_with_markup()  # Calling on this method in ner to NER-ize the text
+
+    # Iterating over the entities in the doc and update the counts in the database
+    cursor = connection.get_cursor()
+    for ent in doc.ents:
+        # Checking if the entity is already in the database
+        cursor.execute("SELECT count FROM entities WHERE label = ? AND text = ?", (ent.label_, ent.text))
+        row = cursor.fetchone()
+        if row is None:
+            # Inserting a new row for the entity if it doesn't already exist
+            cursor.execute("INSERT INTO entities (label, text, count) VALUES (?, ?, 1)", (ent.label_, ent.text))
+        else:
+            # Updating count for the entity if it already exists in the database
+            count = row[0] + 1
+            cursor.execute("UPDATE entities SET count = ? WHERE label = ? AND text = ?", (count, ent.label_, ent.text))
+    connection.commit()
+    return render_template("resultnew.html", text=ner_spacyed_text, entities=doc.ents)
+
+@app.route('/entity_db')
+def get_counts():
+    entity_counts = connection.get_entity_counts()
+    return render_template("entity_db.html", entity_counts=entity_counts)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)  # So that it can run with docker
